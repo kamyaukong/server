3efc07f7096d3adc412de04cb3c6ace192f36c96
kamyaukong <kkong22@my.centennialcollege.ca>
2023-11-15 22:46:18 -0500

    Itinerary fixing

--- routers/itinerary/itinerary.controller.js
+++ routers/itinerary/itinerary.controller.js
@@ -15,41 +15,36 @@ router.post('/', validateItinerary, async (req, res, next) => {
         const newItinerary = new Itinerary(req.body);
         await newItinerary.save()
             .then(itinerary => res.json(itinerary))
-            .catch(err => next(new CustomError(400, 'Save Error: ' + err)));
+            .catch(err => next(new CustomError(400, 'Error occured during Itinerary Save(): ' + err.message)));
         } catch (err) {
-            next(new CustomError(500, "Create itinerary error" + err.message));
+            next(new CustomError(500, 'Itinerary creation error' + err.message));
         }
 });
 
 // GET /api/v1/itinerary - display everything
 router.get('/', async (req, res, next) => {
-    try {
-        const itineraries = await Itinerary.find();
-        res.json(itineraries);
-    } catch (err) {
-        next(err);
-    }
+    Itinerary.find()
+        .then(itineraries => res.json(itineraries))
+        .catch(err => next(new CustomError(400, err.message)));
 });
 
 // GET /api/v1/itinerary/:id - display particular itinerary by ID
 router.get('/:id', async (req, res, next) => {
-    try {
-        const itinerary = await Itinerary.findById(req.params.id);
-        if (!itinerary) return res.status(404).send('Itinerary not found');
-        res.json(itinerary);
-    } catch (err) {
-        next(err);
-    }
+    Itinerary.findById(req.params.id)
+        .then(itinerary => res.json(itinerary))
+        .catch(err => next(new CustomError(400, 'Itinerary [' + req.params.id + '] not found: ' + err.message)));
 });
 
 // PUT /api/v1/itinerary/:id - update particular itinerary by ID
 router.put('/:id', validateItinerary, async (req, res, next) => {
     try {
-        const updatedItinerary = await Itinerary.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedItinerary) return res.status(404).send('Itinerary not found');
-        res.json(updatedItinerary);
+        const itinerary = await Itinerary.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!itinerary) {
+            return next(new CustomError(404, 'Itinerary [' + req.params.id + '] not found for update operation.'));
+        }
+        res.json(itinerary);
     } catch (err) {
-        next(err);
+        next(new CustomError(500, 'Itinerary update error: ' + err.message));
     }
 });
 
@@ -57,10 +52,12 @@ router.put('/:id', validateItinerary, async (req, res, next) => {
 router.delete('/:id', async (req, res, next) => {
     try {
         const itinerary = await Itinerary.findByIdAndDelete(req.params.id);
-        if (!itinerary) return res.status(404).send('Itinerary not found');
-        res.status(200).send('Itinerary deleted');
+        if (!itinerary) {
+            return next(new CustomError(404, 'Itinerary [' + req.params.id + '] not found for delete operation.'));
+        }
+        res.status(200).send('Itinerary [' + req.params.id +  '] has been deleted');
     } catch (err) {
-        next(err);
+        next(new CustomError(500, 'Itinerary deletion error: ' + err.message));
     }
 });
 
